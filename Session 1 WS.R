82aaacfcf0ab1f6fc074b7a28a0a59d7b07c0ead
sandeep patel <sandeep.patel@live.vu.edu.au>
2022-02-22 21:57:49 +1100

    session one workshop

---
+++ Session 1 WS.R
@@ -0,0 +1,18 @@
+150/3
+a<-10
+b <- "Cool"
+b
+a
+summary(c)
+c<-c(3,4,5,6,7)
+summary(c)
+install.packages("tidyverse")
+library(tidyverse)
+rm(b)
+data(mtcars)
+data(mtcars)
+
+data(mtcars)
+head(mtcars)
+summary(mtcars)
+glimpse(mtcars)
